5d68277a971dc1a6e2623d163f97c782a907b712
ddemille1 <71284757+ddemille1@users.noreply.github.com>
2022-05-12 14:08:56 -0400

    Added all of the content for first draft

---
+++ chamber/js/script.js
@@ -0,0 +1,15 @@
+/*code for getting current date, in format Wednesday, May 11, 2022*/
+let now = new Date();
+let fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+
+document.getElementById('date').textContent = fulldate;
+
+/* code for getting copywright date*/
+let c = new Date()
+let year = c.getFullYear()
+document.getElementById("copyright").textContent = year
+
+/*code for getting last modified*/
+let d = new Date(document.lastModified)
+let date = `Last Updated: ${d.getMonth()+1}/${d.getDate()}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+document.getElementById("lastmodified").textContent = date
