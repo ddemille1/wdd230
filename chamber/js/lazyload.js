dab35226de08f0feabd1068a4d5d7dd9406cfb11
ddemille1 <71284757+ddemille1@users.noreply.github.com>
2022-06-04 09:21:13 -0400

    finished discover :)

--- chamber/js/lazyload.js
+++ chamber/js/lazyload.js
@@ -1,4 +1,4 @@
-if ('loading' in HTMLImageElement.prototype) {
+/*if ('loading' in HTMLImageElement.prototype) {
     const images = document.querySelectorAll(".lazyLoad");
     images.forEach(img => {
         img.src = img.dataset.src;
@@ -9,3 +9,39 @@ if ('loading' in HTMLImageElement.prototype) {
     script.src = "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js";
     document.body.appendChild(script);
 }
+*/
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+const imgOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -50px 0px"
+};
+
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} 
+
+else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
+
