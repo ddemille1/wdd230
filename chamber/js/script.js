66c1934e56bf9a5a8e49264dfbbee6788b2d4642
ddemille1 <71284757+ddemille1@users.noreply.github.com>
2022-06-09 17:26:15 -0400

    finished join page

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -2,7 +2,11 @@
 let now = new Date();
 let fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
 
+/*putting date in date section at the top*/ 
 document.getElementById('date').textContent = fulldate;
+/*putting date in hidden input field in join page*/
+document.querySelector('#visitTime').value = fulldate;
+
 
 /* code for getting copywright date*/
 let c = new Date()
@@ -16,7 +20,7 @@ document.getElementById("lastmodified").textContent = date
 
 
 /*code for responsive hamburger menu*/
-
+/** This needs to be modified to querySelectorAll then something else to run through the new list. right now it is only applying to the last instances of .hamburgerBtn and .navigation. Currently the dropdown only works on the last page -- the join page*/
 const hambutton = document.querySelector('.hamburgerBtn');
 const mainnav = document.querySelector('.navigation');
 
