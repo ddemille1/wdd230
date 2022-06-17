2043a82e1c80b9672f61d64c9a08d2d631a15768
ddemille1 <ddemille1@gmail.com>
2022-06-17 16:56:08 -0400

    roughdraft list and grid in all views

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -37,3 +37,18 @@ function displayCompanies (company){
 
     document.querySelector('div.cards').appendChild(card);
 }
+
+//to get buttons working//
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector(".cards");
+
+gridbutton.addEventListener("click", () => {
+    display.classList.add("cards");
+    display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", () => {
+    display.classList.add("list");
+    display.classList.remove("cards");
+})
