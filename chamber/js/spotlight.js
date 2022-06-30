14b2e9724e28b710c212844ad4a0d11dfca3a112
ddemille1 <71284757+ddemille1@users.noreply.github.com>
2022-06-30 17:10:40 -0400

    solved spotlight repeat problem

--- chamber/js/spotlight.js
+++ chamber/js/spotlight.js
@@ -9,18 +9,40 @@ fetch(requestInfo)
         const companies = jsonObject['companies']
         console.log(companies);
         //function to get filtered array
-        const filteredCos = [companies[2], companies[3], companies[6], companies[7]]
-        console.log(filteredCos);
+        var filteredCos = [companies[2], companies[3], companies[6], companies[7]]
+        console.table(filteredCos);
 
         //get 3 random companies from filtered array
+    ///1st company    
         const randomCompany = getRandomCo(filteredCos);
         console.log(randomCompany);
-        
+           //find index of randomly selected company
+        const randomCompanyIndex = filteredCos.indexOf(randomCompany)
+        console.log(randomCompanyIndex);
+
+            //remove selected company from list so it doesn't repeat
+        filteredCos.splice(randomCompanyIndex, 1)
+        console.table(filteredCos)
+
+    ///2nd company    
         const randomCompany1 = getRandomCo(filteredCos);
         console.log(randomCompany1);
-        
+
+           //find index of randomly selected company
+           const randomCompanyIndex1 = filteredCos.indexOf(randomCompany1)
+           console.log(randomCompanyIndex1);
+
+        filteredCos.splice(randomCompanyIndex1, 1)
+        console.table(filteredCos)
+    ///3rd company    
         const randomCompany2 = getRandomCo(filteredCos);
         console.log(randomCompany2);
+            //find index of randomly selected company
+           const randomCompanyIndex2 = filteredCos.indexOf(randomCompany2)
+           console.log(randomCompanyIndex2);
+
+        filteredCos.splice(randomCompanyIndex2, 1)
+        console.table(filteredCos)
         
     //functions to display random companies on page
         //for spotlight 1
